dbb14ab4965b9a4d2ae601a2d49b01b4588cee1d
whowouldwin <leranetwork@gmail.com>
2023-07-01 15:32:27 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,49 @@
+// function* fibonacciGenerator() {
+//   let previous = 0;
+//   let current = 1;
+//   while (true) {
+//     yield current;
+//     [previous, current] = [current, previous + current];
+//   }
+// }
+//
+// // Использование генератора чисел Фибоначчи
+// const generator = fibonacciGenerator();
+// console.log(generator.next().value); // 1
+// console.log(generator.next().value); // 1
+// console.log(generator.next().value); // 2
+// console.log(generator.next().value); // 3
+// // ...
+//
+
+
+
+
+// const testString = 'ffff';
+// repeatedString = testString.repeat(4);
+// console.log(repeatedString);
+//
+//
+
+
+
+// let testNumberFloor = Math.floor(-3.5);
+// let testNumberRound = Math.round(-3.5);
+// let testNumberCeil = Math.ceil(-3.5);
+// console.log(testNumberFloor)
+// console.log(testNumberCeil)
+// console.log(testNumberRound)
+
+
+
+
+
+// let testArray = ['a', 'b', 'c'];
+// testArray.unshift(testArray.pop());
+// console.log(testArray);
+
+Promise.race()
+
+
+
+
